9dd2e7db8e52d04a83f6a4a89efd83cd566a35dd
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-11-16 02:56:16 +0000

    Used a font from google api

---
+++ components/fonts.jsx
@@ -0,0 +1,10 @@
+import { Global } from "@emotion/react";
+
+const Fonts = () => (
+
+    <Global
+    styles={'https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;700&display=swap'}
+    />
+)
+
+export default Fonts
