da7bba2aea89b12da050fd0021c0fcfd310d4e0d
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-12-08 01:56:53 -0800

    finished transitioning pages

---
+++ components/voxel-dog.jsx
@@ -0,0 +1,19 @@
+import { useState, useEffect, useRef, useCallback  } from "react"
+import { Box, Spinner } from '@chakra-ui/react'
+import * as THREE from 'three'
+import { OrbitControl } from 'three/examples/jsm/controls/OrbitControls
+import { loadGLTFModel } from '../lib/model'
+
+function easeOutCirc(x){
+  return Math.sqrt(1 - Math.pow(x - 1, 4))
+}
+
+const VoxelDog = () => {
+  const refContainer = useRef()
+
+  return (
+    <Box ref={refContainer}>
+      Dog!!
+    </Box>
+  )
+}
