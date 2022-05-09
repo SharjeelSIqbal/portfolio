187114ef4bf0768157a075b6e15bc9bce8aa05d6
SharjeelSIqbal <SharjeelSIqbal@gmail.com>
2022-05-09 06:44:15 -0700

    Added node mailer to the portfolio, set up response bodies for sender and user

--- components/contact-me.jsx
+++ components/contact-me.jsx
@@ -5,6 +5,7 @@ import { FormInput, FormSubmit, FormTextarea } from './form'
 import { IoMdSend } from 'react-icons/io'
 import Section from './section'
 import NoSsr from './no-ssr'
+import axios from 'axios'
 
 
 const contactMeGreeting = "Hello Sharjeel, I'd love to connect and talk with you about <opportunity>. Please contact me at <email/phone>. I'm looking forward to hearing back from you!"
@@ -13,6 +14,7 @@ const initialState = {
   email: '',
   textarea: '',
   subject: '',
+  phone: '',
 }
 
 export const ContactMe = () => {
@@ -34,14 +36,23 @@ export const ContactMe = () => {
     }
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    axios.post("/api/mail", {
+      body: values
+    }).then( result => console.log(result))
+  }
+
+
   return (
     <Section delay={0.5}>
       <Heading as="h4" variant="section-title">
         Contact Me!
       </Heading>
-      <FormControl>
+      <form type="submit" onSubmit={handleSubmit}>
         <FormInput onChange={handleInputChange} placeholder="Email" type="email" size="md" name="email" value={values.email} />
         <FormInput placeholder="Subject" onChange={handleInputChange} size="md" value={values.subject} name="subject" />
+        <FormInput placeholder="Phone" onChange={handleInputChange} size="md" value={values.phone} name="phone" />
         <FormTextarea
           name="textarea"
           onChange={handleInputChange}
@@ -49,19 +60,16 @@ export const ContactMe = () => {
           placeholder={contactMeGreeting + " Press 'Enter' to autofill form."}
           variant="filled"
           size="xl"
-          value={values.textarea
-          }>
-
+          value={values.textarea}>
         </FormTextarea>
         <Stack direction="row" justify="end">
 
         <NoSsr>
-
-          <FormSubmit rightIcon={<IoMdSend />} colorScheme="teal" variant="solid">Send</FormSubmit>
+          <FormSubmit rightIcon={<IoMdSend />} colorScheme="teal" type="submit" variant="solid">Send</FormSubmit>
         </NoSsr>
 
         </Stack>
-      </FormControl>
+      </form>
     </Section>
 )
 }
